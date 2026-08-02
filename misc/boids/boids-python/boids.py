dbfce8dcbf0079769d6c542db9059482a0d0042f
nolawimelese <nolawimelese@gmail.com>
2026-08-02 01:57:26 -0400

    added boids class

--- misc/boids/boids-python/boids.py
+++ misc/boids/boids-python/boids.py
@@ -9,10 +9,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill((17, 17, 17))
-
+            
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(24)
 
 pygame.quit()
+
+# use pygame.Vector2 for position and velocity
+# methods: init, update, and draw
+# flock should be updated, and then drawn in a loop
+
+class Boid:
+    def __init__(self, x, y, vx, vy):
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(vx, vy)
+
+    def update(self):
+        self.position += self.velocity
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255,255,255), self.position, 1)
