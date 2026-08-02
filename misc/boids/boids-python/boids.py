534f417aaaa50e087538ee53813683beea69a3ed
nolawimelese <nolawimelese@gmail.com>
2026-08-02 02:49:43 -0400

    updated update(), added separation, added adjustable parameters, removed streaking

--- misc/boids/boids-python/boids.py
+++ misc/boids/boids-python/boids.py
@@ -1,31 +1,66 @@
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-clock = pygame.time.Clock()
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            
-    pygame.display.flip()
-    clock.tick(24)
-
-pygame.quit()
-
 # use pygame.Vector2 for position and velocity
 # methods: init, update, and draw
 # flock should be updated, and then drawn in a loop
+
+# parameters
+# neighbor radius: how close birds have to be to be classified as neighbors
+neighbor_radius = 25
+sep_weight = .5
+speed_limit = 5
+boid_size = 5
 
 class Boid:
     def __init__(self, x, y, vx, vy):
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(vx, vy)
+        self.neighbors = []
 
-    def update(self):
+    def separation(self, neighbors):
+        sep = pygame.Vector2()
+        for b in neighbors:
+            if (self.position != b.position): # just in case 2 boids are in the same pos
+                sep += (self.position - b.position)/self.position.distance_to(b.position)**2
+        if len(neighbors) == 0:
+            return sep
+        return sep / len(neighbors)
+
+    def alignment(self, neighbors):
+        pass
+
+    def cohesion(self, neighbors):
+        pass
+
+    def update(self, flock):
+        self.neighbors = [b for b in flock if b is not self and self.position.distance_to(b.position) < neighbor_radius]
+        self.velocity += (sep_weight * self.separation(self.neighbors))
+        if (self.velocity.magnitude() > speed_limit):
+            self.velocity.scale_to_length(speed_limit)
         self.position += self.velocity
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255,255,255), self.position, 1)
+        pygame.draw.circle(screen, (255,255,255), self.position, boid_size)
+
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+running = True
+
+flock = [Boid(100,100,1,1), Boid(110,100,1,1), Boid(100,110,1,1)]
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0,0,0))
+
+    for boid in flock:
+        boid.update(flock)
+        boid.draw(screen)
+    
+    pygame.display.flip()
+    clock.tick(24)
+
+pygame.quit()
